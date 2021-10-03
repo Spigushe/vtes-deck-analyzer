edba1b762b1386b4dd2530cc06cc7caea0f52392
Spigushe <martin.cuchet@gmail.com>
2021-10-03 23:42:36 +0200

    Creating temporary dir
    Create a dir in the studied file directory and deletes it

--- deck_analyzer/__init__.py
+++ deck_analyzer/__init__.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import logging
+import shutil
 
 
 from .deck import Deck
@@ -14,8 +15,20 @@ def main():
     if os.path.exists(fn):
         logging.info("File exists: " + os.path.basename(fn))
 
+        # Create a directory to temp save files
+        dir_name = os.path.basename(fn)[: os.path.basename(fn).rfind(".")]
+        dir_name = os.path.join(os.path.dirname(fn), dir_name)
+        if not os.path.exists(dir_name):
+            os.mkdir(dir_name)
+
         deck = Deck()
         deck.from_file(fn)
+
+        # Delete the temp dir
+        try:
+            shutil.rmtree(dir_name)
+        except OSError as e:
+            print("Error: %s - %s." % (e.filename, e.strerror))
 
     else:
         logging.error("File does not exists: " + os.path.basename(fn))
