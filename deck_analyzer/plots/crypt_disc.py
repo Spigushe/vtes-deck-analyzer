ea1dc8eee07dafa7e2a58673b69555152554515b
Spigushe <martin.cuchet@gmail.com>
2021-10-04 08:36:19 +0200

    Changing Sort style
    Graph now sorts discipline from most present to less present

--- deck_analyzer/plots/crypt_disc.py
+++ deck_analyzer/plots/crypt_disc.py
@@ -12,6 +12,19 @@ def check_level(discipline: str) -> str:
     return "sup"
 
 
+def sort_disc(table: dict) -> dict:
+    temp = []
+    for key in table.keys():
+        temp.append((key, table[key]["tot"], table[key]["sup"], table[key]["inf"]))
+    # Default sorting: alphabetically
+    temp = sorted(temp, key=lambda t: t[0])
+    # Secondary sorting: quantity of superior DESC
+    temp = sorted(temp, key=lambda t: t[2], reverse=True)
+    # Primary sorting: quantity of total DESC
+    temp = sorted(temp, key=lambda t: t[1], reverse=True)
+    return temp
+
+
 def crypt_disc(deck: Deck, path):
     if not vtes.VTES:
         vtes.VTES.load()
@@ -24,16 +37,18 @@ def crypt_disc(deck: Deck, path):
                     disc[d.lower()] = {
                         "inf": 0,
                         "sup": 0,
+                        "tot": 0,
                     }
                 disc[d.lower()][check_level(d)] += item[key]
-
-    keys = [k for k, v in disc.items()]
-    keys.sort()
+                disc[d.lower()]["tot"] += item[key]
+    keys = sort_disc(disc)
 
     inf, sup = [], []
+    labels = []
     for k in keys:
-        inf.append(disc[k]["inf"] * -1)
-        sup.append(disc[k]["sup"])
+        inf.append(disc[k[0]]["inf"] * -1)
+        sup.append(disc[k[0]]["sup"])
+        labels.append(k[0].upper())
 
     fig, ax = plt.subplots()
     N = len(keys)
@@ -42,7 +57,7 @@ def crypt_disc(deck: Deck, path):
     inf = ax.bar(ind, inf, width=0.8, label="Inferior")
     ax.axhline(0, color="grey", linewidth=0.8)
     ax.set_xticks(ind)
-    ax.set_xticklabels(keys)
+    ax.set_xticklabels(labels)
     ax.legend()
 
     plt.savefig(os.path.join(path, "crypt_disc.pdf"))
