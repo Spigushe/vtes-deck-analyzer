8cff253298af156affc6c871a4ac7b79611d17a7
Spigushe <martin.cuchet@gmail.com>
2021-10-04 09:08:38 +0200

    Calling Vote Curve

--- deck_analyzer/__init__.py
+++ deck_analyzer/__init__.py
@@ -8,6 +8,7 @@ import shutil
 from .deck import Deck
 from .plots.crypt_curve import crypt_curve
 from .plots.crypt_disc import crypt_disc
+from .plots.crypt_votes import crypt_votes
 
 
 def main():
@@ -31,6 +32,8 @@ def main():
         logging.info("Crypt curve generated")
         crypt_disc(deck, dir_name)
         logging.info("Discipline curve generated")
+        crypt_votes(deck, dir_name)
+        logging.info("Vote curve generated")
 
         # Delete the temp dir
         try:
