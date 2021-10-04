a8b4206903e368c3c75b1f2c9c86cdd61d940cb8
Spigushe <martin.cuchet@gmail.com>
2021-10-04 09:53:54 +0200

    Adding Disc Symbols
    Adding discipline symbols to the chart
    https://stackoverflow.com/questions/44246650/add-image-annotations-to-bar-plots/44264051#44264051

--- deck_analyzer/plots/crypt_disc.py
+++ deck_analyzer/plots/crypt_disc.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 import os
 import matplotlib.pyplot as plt  # https://matplotlib.org/stable/index.html
-import numpy as np
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+
+# https://stackoverflow.com/questions/44246650/add-image-annotations-to-bar-plots/44264051#44264051
+import shutil
+import requests
 from krcg import vtes
 from ..deck import Deck
 
@@ -23,6 +27,38 @@ def sort_disc(table: dict) -> dict:
     # Secondary sorting: quantity of superior DESC
     temp = sorted(temp, key=lambda t: t[2], reverse=True)
     return temp
+
+
+def get_disc(d: str, dir_name: str):
+    # File address
+    path = os.path.join(dir_name, d.lower() + ".png")
+    # Download discipline
+    url = "https://static.krcg.org/png/disc/sup/{}.png".format(d.lower())
+    response = requests.get(url, stream=True)
+    with open(path, "wb") as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
+    # Return picture
+    im = plt.imread(path)
+    return im
+
+
+def offset_image(coord, name, ax, dir_name):
+    img = get_disc(name, dir_name)
+    im = OffsetImage(img, zoom=0.025)
+    im.image.axes = ax
+
+    ab = AnnotationBbox(
+        im,
+        (coord, 0),
+        xybox=(0.0, -16.0),
+        frameon=False,
+        xycoords="data",
+        boxcoords="offset points",
+        pad=0,
+    )
+
+    ax.add_artist(ab)
 
 
 def crypt_disc(deck: Deck, path):
@@ -51,14 +87,16 @@ def crypt_disc(deck: Deck, path):
         labels.append(k[0].upper())
 
     fig, ax = plt.subplots()
-    N = len(keys)
-    ind = np.arange(N)
-    sup = ax.bar(ind, sup, width=0.8, label="Superior")
-    inf = ax.bar(ind, inf, width=0.8, label="Inferior")
+    sup = ax.bar(range(len(labels)), sup, width=0.8, label="Superior")
+    inf = ax.bar(range(len(labels)), inf, width=0.8, label="Inferior")
     ax.axhline(0, color="grey", linewidth=0.8)
-    ax.set_xticks(ind)
+
+    ax.set_xticks(range(len(labels)))
     ax.set_xticklabels(labels)
-    ax.legend()
+    ax.tick_params(axis="x", which="major")
+
+    for i, c in enumerate(labels):
+        offset_image(i, c, ax, path)
 
     plt.savefig(os.path.join(path, "crypt_disc.pdf"))
     plt.savefig(os.path.join(path, "crypt_disc.svg"))
